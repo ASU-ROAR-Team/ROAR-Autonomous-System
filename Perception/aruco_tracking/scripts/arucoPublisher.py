d688bf3817b16b9043a195ebd469f20e9dcbc164
Carol Rimoun <academiccarol@gmail.com>
2025-02-15 21:13:29 +0200

    aruco_tracking package

---
+++ Perception/aruco_tracking/scripts/arucoPublisher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
+from cv_bridge import CvBridge
+from cv_bridge.boost import create_images
+from msg import LandmarkArray"""
